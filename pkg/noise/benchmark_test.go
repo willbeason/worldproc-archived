ae749589dbbd8611273644b8525a45c6c38073d0
Will Beason <willbeason@gmail.com>
2019-04-02 22:47:34 -0700

    add fill test

--- pkg/noise/benchmark_test.go
+++ pkg/noise/benchmark_test.go
@@ -3,6 +3,7 @@ package noise
 import (
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func randP() pos {
@@ -14,5 +15,16 @@ var px = randP()
 func BenchmarkValue_V(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		n.V(px.x, px.y)
+	}
+}
+
+var (
+	n2 = Value{}
+	src = rand.NewSource(time.Now().UnixNano())
+)
+
+func BenchmarkValue_Fill(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		n2.Fill(src)
 	}
 }
